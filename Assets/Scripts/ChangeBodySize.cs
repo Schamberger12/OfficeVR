176649224f7f965f37c0c6fd3f97ad72f22dfec3
Schamberger12 <32084866+Schamberger12@users.noreply.github.com>
2019-10-18 15:23:57 -0500

    changed some stuff

---
+++ Assets/Scripts/ChangeBodySize.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeBodySize : MonoBehaviour
+{
+
+
+    [SerializeField]
+    GameObject simulatedCamera;
+
+    //[SerializeField]
+    //GameObject VRCamera;
+
+    //[SerializeField]
+    //GameObject bodyRepresentation;
+
+    float timer = 0f;
+
+    float maxScale = 5f;
+    float minScale = 0.3f;
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("up"))
+        {
+
+            simulatedCamera.transform.localScale += new Vector3(1, 1, 1) * Time.deltaTime;
+
+            if (simulatedCamera.transform.localScale == new Vector3(minScale, minScale, minScale))
+            {
+                simulatedCamera.transform.localScale = new Vector3(minScale, minScale, minScale);
+            }
+        }
+        if (Input.GetKey("down"))
+        {
+            simulatedCamera.transform.localScale -= new Vector3(1, 1, 1) * Time.deltaTime;
+            if (simulatedCamera.transform.localScale == new Vector3(maxScale, maxScale, maxScale))
+            {
+                simulatedCamera.transform.localScale = new Vector3(maxScale, maxScale, maxScale);
+            }
+        }
+
+        if (Input.GetKeyUp("up"))
+        {
+            timer = 0f;
+        }
+        if (Input.GetKeyUp("down"))
+        {
+            timer = 0f;
+        }
+        if (Input.GetKeyDown("up"))
+        {
+            timer = 0f;
+        }
+        if (Input.GetKeyDown("down"))
+        {
+            timer = 0f;
+        }
+    }
+}
--- Assets/Scripts/ChangeSize.cs
+++ Assets/Scripts/ChangeSize.cs
@@ -4,28 +4,66 @@ using UnityEngine;
 
 public class ChangeSize : MonoBehaviour
 {
+    [SerializeField]
+    GameObject room;
+
     //[SerializeField]
-    //GameObject room;
+    //GameObject charPoint;
 
-    //float timer = 0f;
+    //[SerializeField]
+    //GameObject simulatedCamera;
 
-    //float maxScale = 5f;
-    //float minScale = 0.3f; 
+    //[SerializeField]
+    //GameObject VRCamera;
+
+    //[SerializeField]
+    //GameObject bodyRepresentation; 
+
+    float timer = 0f;
+
+    float maxScale = 5f;
+    float minScale = 0.3f;
 
 
 
 
-    //// Update is called once per frame
-    //void Update()
-    //{
-    //    if (Input.GetKey("up"))
-    //    {
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("up"))
+        {
 
-    //        room.scale
-    //    }
-    //    if (Input.GetKey("down"))
-    //    {
+            room.transform.localScale += new Vector3(1, 1, 1) * Time.deltaTime; 
 
-    //    }
-    //}
+            if(room.transform.localScale == new Vector3(minScale, minScale, minScale))
+            {
+                room.transform.localScale = new Vector3(minScale, minScale, minScale); 
+            }
+        }
+        if (Input.GetKey("down"))
+        {
+            room.transform.localScale -= new Vector3(1, 1, 1) * Time.deltaTime;
+            if (room.transform.localScale == new Vector3(maxScale, maxScale, maxScale))
+            {
+                room.transform.localScale = new Vector3(maxScale, maxScale, maxScale);
+            }
+        }
+
+        if (Input.GetKeyUp("up"))
+        {
+            timer = 0f; 
+        }
+        if (Input.GetKeyUp("down"))
+        {
+            timer = 0f; 
+        }
+        if (Input.GetKeyDown("up"))
+        {
+            timer = 0f; 
+        }
+        if (Input.GetKeyDown("down"))
+        {
+            timer = 0f; 
+        }
+    }
 }
